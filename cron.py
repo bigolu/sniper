4628034bd1089692e72951e954770e6a6d5e71a7
Biggie Emmanuel <bigolu96@gmail.com>
2016-08-15 21:42:03 +0000

    fix link in email

--- cron.py
+++ cron.py
@@ -70,7 +70,7 @@ def notify(snipe, index):
         }
 
         # build the url for prepopulated form
-        url = 'http://sniper.bigolu.me/?%s' % (urllib.urlencode(attributes))
+        url = 'http://sniper.biggi.es/?%s' % (urllib.urlencode(attributes))
 
         register_url = 'https://sims.rutgers.edu/webreg/editSchedule.htm?login=cas&semesterSelection=92016&indexList=%s' % (index)
 
